1057e71fe1f89a217de3174b0719253ce751412e
unknown <dangxuanduong1109@gmail.com>
2023-03-07 13:11:03 +0700

    first commit

---
+++ Subject.java
@@ -0,0 +1,31 @@
+/** 
+ * Nhom 5 HUFI
+ * interface subject 
+ *
+ * -
+ */
+public interface Subject {
+    /** 
+     *
+     * method create new song 
+     *
+     * @param observer add song
+     */
+    void addObserver(Observer observer);
+
+    /** 
+     *
+     * method update song 
+     *
+     * @param observer update song
+     */
+    void removeObserver(Observer observer);
+
+    /** 
+     *
+     * method notify new song publish 
+     *
+     * @param songName song name
+     */
+    void notifyObservers(String songName);
+}
---
+++ Subscriber.java
@@ -0,0 +1,9 @@
+/** 
+ * Nhom 5 HUFI
+ * Subscriber interface update new magazine
+ *
+*  
+*/
+public interface  Subscriber {
+    void update(String magazine);
+}
